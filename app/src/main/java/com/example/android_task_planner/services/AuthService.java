fbc5ae0ab7467f5d5a5e8bf14f335a3c1d464925
Johann Bogota <johann.bogota@mail.escuelaing.edu.co>
2021-11-22 15:51:07 -0500

    implementation service interface

---
+++ app/src/main/java/com/example/android_task_planner/services/AuthService.java
@@ -0,0 +1,16 @@
+package com.example.android_task_planner.services;
+
+
+import com.example.android_task_planner.network.dto.LoginDto;
+import com.example.android_task_planner.network.dto.TokenDto;
+
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import rx.Observable;
+
+
+public interface AuthService {
+    @POST("auth")
+    Observable<TokenDto> auth(@Body LoginDto loginDto);
+
+}
---
+++ app/src/main/java/com/example/android_task_planner/services/TaskService.java
@@ -0,0 +1,33 @@
+package com.example.android_task_planner.services;
+
+
+import com.example.android_task_planner.network.dto.TaskDto;
+
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface TaskService {
+
+    @GET("api/user/all")
+    Observable listUsers();
+
+    @GET("api/task/all")
+    Observable listTasks();
+
+    @POST("api/task")
+    Observable<TaskDto> saveTask(@Body TaskDto taskDto);
+
+    @PUT("api/task/{id}")
+    Observable<TaskDto> updateTask(@Path("id") String id, @Body TaskDto taskDto);
+
+    @POST("api/task/{id}/assign/{assignedTo}")
+    Observable<TaskDto> assignTask(@Path ("id") String id, @Path("assignedTo") String assignedTo, @Body TaskDto taskDto);
+
+    @DELETE("api/task/{id}")
+    Observable<TaskDto> deleteTask(@Path ("id") String id,@Body TaskDto taskDto);
+}
